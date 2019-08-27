a65fbd14d10f0f62b066bb3421e8ce35fc07573a
Xu Han <xhs400@cs.unc.edu>
2019-08-27 00:10:56 -0400

    update dataloader

--- data_loaders/pseudo_3D.py
+++ data_loaders/pseudo_3D.py
@@ -9,14 +9,13 @@ class Pseudo3DDataset(Dataset):
     def __init__ (self, mode='training', type='normal'):
         root_folder = '/playpen/xhs400/Research/data/data_for_pregis_net'
 
-        oasis_affined_folder = os.path.join(root_folder, 'oasis_affined')
-        oasis_brain_folder = os.path.join(oasis_affined_folder, 'oasis_aff_normed')
-        #oasis_label_folder = os.path.join(oasis_affined_folder, 'labels')
+        oasis_affined_folder = os.path.join(root_folder, 'oasis_3', 'affined')
+        oasis_brain_folder = os.path.join(oasis_affined_folder, 'normalized', 'pseudo_not_used')
 
         atlas_folder = os.path.join(root_folder, 'atlas_folder')
         atlas_file = os.path.join(atlas_folder, 'atlas.nii.gz')
 
-        pseudo_folder = os.path.join(root_folder, 'pseudo_3D')
+        pseudo_folder = os.path.join(root_folder, 'pseudo', 'pseudo_3D')
         pseudo_tumor_folder = os.path.join(pseudo_folder, 'tumor')
         pseudo_no_tumor_folder = os.path.join(pseudo_folder, 'no_tumor')
 
@@ -33,30 +32,18 @@ class Pseudo3DDataset(Dataset):
 
         self.mode = mode
         num_of_all_files = len(image_files)
-        num = num_of_all_files // 40
+        num = num_of_all_files // 10
 
         # split the data for normal training and abnormal training, so that they don't overlap for sanity.
-        if type == 'normal':
-            if mode == 'training':
-                self.image_files = image_files[6*num:20*num]
-            elif mode == 'validation':
-                self.image_files = image_files[3*num:6*num]
-            elif mode == 'testing':
-                self.image_files = image_files[0:3*num]
-            else:
-                raise ValueError('Mode not supported')
-        elif type == 'no_tumor' or type == 'tumor':
-            if mode == 'training':
-                self.image_files = image_files[26*num:40*num]
-            elif mode == 'validation':
-                self.image_files = image_files[23*num:26*num]
-            elif mode == 'testing':
-                self.image_files = image_files[20:23*num]
-            else:
-                raise ValueError('Mode not supported')
-
+        if mode == 'training':
+            self.image_files = image_files[4*num:10*num]
+        elif mode == 'validation':
+            self.image_files = image_files[2*num:4*num]
+        elif mode == 'testing':
+            self.image_files = image_files[0:2*num]
         else:
-            raise ValueError("Type not supported")
+            raise ValueError('Mode not supported')
+
 
         self.atlas_file = atlas_file
         image_io = py_fio.ImageIO()

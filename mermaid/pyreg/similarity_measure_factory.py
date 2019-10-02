f8f56fdc2a0e938abecffc457aac71feec1db9da
Xu Han <xhs400@biag-w05.cs.unc.edu>
2019-10-02 16:23:40 -0400

    update dataloader

--- mermaid/pyreg/similarity_measure_factory.py
+++ mermaid/pyreg/similarity_measure_factory.py
@@ -602,6 +602,7 @@ class CustLNCCSimilarity(SimilarityMeasure):
 
     """
 
+
     def __init__(self, spacing, params):
         super(CustLNCCSimilarity, self).__init__(spacing, params)
         self.dim = len(spacing)
@@ -612,38 +613,16 @@ class CustLNCCSimilarity(SimilarityMeasure):
             "kernel size, ratio of input size")]
         self.kernel_weight_ratio = params['similarity_measure']['lncc'][
             ('kernel_weight_ratio', [[0.1, 0.3, 0.6], [0.3, 0.7], [1.]], "kernel size, ratio of input size")]
-        self.stride = params['similarity_measure']['lncc'][
-            ('stride', [1. / 4, 1. / 4, 1. / 4], "step size, responded with ratio of kernel size")]
-        self.dilation = params['similarity_measure']['lncc'][
-            ('dilation', [1, 2, 2], "dilation param, responded with ratio of kernel size")]
+        self.strides = params['similarity_measure']['lncc'][(
+            'stride', [[1. / 4, 1. / 4, 1. / 4], [1. / 4, 1. / 4], [1. / 4]],
+            "step size, responded with ratio of kernel size")]
+        self.dilations = params['similarity_measure']['lncc'][
+            ('dilation', [[2, 2, 2], [2, 2], [1]], "dilation param, responded with ratio of kernel size")]
         if self.resol_bound[0] > -1:
             assert len(self.resol_bound) + 1 == len(self.kernel_size_ratio)
             assert len(self.resol_bound) + 1 == len(self.kernel_weight_ratio)
-            assert max(len(kernel) for kernel in self.kernel_size_ratio) == len(self.stride)
-            assert max(len(kernel) for kernel in self.kernel_size_ratio) == len(self.dilation)
-
-    def compute_similarity_multiC(self, I0, I1, I0Source=None, phi=None):
-        sz0 = I0.size()[0]
-        sz1 = I1.size()[0]
-        assert (sz0 == sz1 or sz0 == sz1 - 1)
-
-        # last channel of target image is similarity mask
-        num_of_labels = sz0 - 1
-        mask = None
-        if (sz0 == sz1 - 1):
-            mask = I1[-1, ...]
-
-        sim = 0.0
-        sim = sim + self.compute_similarity(I0[0, ...], I1[0, ...], isLabel=False, similarity_mask=None)
-
-        if num_of_labels > 0:
-            I0_labels = I0[1:, ...]
-            I1_labels = I1[1:, ...]
-            for nrL in range(num_of_labels):
-                sim = sim + self.compute_similarity(I0_labels[nrL, ...], I1_labels[nrL, ...], isLabel=True,
-                                                    similarity_mask=mask)
-
-        return AdaptVal(sim / self.sigma ** 2)
+            assert len(self.resol_bound) + 1 == len(self.strides)
+            assert len(self.resol_bound) + 1 == len(self.dilations)
 
     def __stepup(self, img_sz):
         max_scale = min(img_sz)
@@ -651,10 +630,14 @@ class CustLNCCSimilarity(SimilarityMeasure):
             if max_scale >= bound:
                 self.kernel = [int(max_scale * kz) for kz in self.kernel_size_ratio[i]]
                 self.weight = self.kernel_weight_ratio[i]
+                self.stride = self.strides[i]
+                self.dilation = self.dilations[i]
                 break
         if max_scale < self.resol_bound[-1]:
             self.kernel = [int(max_scale * kz) for kz in self.kernel_size_ratio[-1]]
             self.weight = self.kernel_weight_ratio[-1]
+            self.stride = self.strides[-1]
+            self.dilation = self.dilations[-1]
 
         self.num_scale = len(self.kernel)
         self.kernel_sz = [[k for _ in range(self.dim)] for k in self.kernel]
@@ -669,6 +652,32 @@ class CustLNCCSimilarity(SimilarityMeasure):
             self.conv = F.conv3d
         else:
             raise ValueError(" Only 1-3d support")
+
+    
+    
+    def compute_similarity_multiC(self, I0, I1, I0Source=None, phi=None):
+        sz0 = I0.size()[0]
+        sz1 = I1.size()[0]
+        assert (sz0 == sz1 or sz0 == sz1 - 1)
+
+        # last channel of target image is similarity mask
+        num_of_labels = sz0 - 1
+        mask = None
+        if (sz0 == sz1 - 1):
+            mask = I1[-1, ...]
+
+        sim = 0.0
+        sim = sim + self.compute_similarity(I0[0, ...], I1[0, ...], isLabel=False, similarity_mask=None)
+        if num_of_labels > 0:
+            I0_labels = I0[1:, ...]
+            I1_labels = I1[1:, ...]
+            for nrL in range(num_of_labels):
+                sim = sim + self.compute_similarity(I0_labels[nrL, ...], I1_labels[nrL, ...], isLabel=True,
+                                                    similarity_mask=mask)
+
+        return AdaptVal(sim / self.sigma ** 2)
+
+
 
     def compute_similarity(self, I0, I1, I0Source=None, phi=None, isLabel=False, similarity_mask=None):
         """
